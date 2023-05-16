a25ece092d14fb7e42f604c432716848c1c48de4
Nevish-302 <shiven302@gmail.com>
2023-05-17 04:20:22 +0530

    initPagesSubAll

--- assets/libs/defVar.py
+++ assets/libs/defVar.py
@@ -1,11 +1,13 @@
 class Variables:
-    appWidth=900
-    appHeight=600
-    appBackgroundTheme='green'
+    appWidth = 900
+    appHeight = 600
+    appBackgroundTheme = '#6200ee'
+    appBackgroundCompliment = '#3700b3'
+
 
 class fontstyle:
-    titleFontSize=44
-    NormalfontSize=10
-    smallFontSize=5
+    titleFontSize = 44
+    NormalfontSize = 10
+    smallFontSize = 5
 
-    fontText='Courier'
+    fontText = 'Courier'
